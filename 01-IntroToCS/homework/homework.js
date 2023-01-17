93fb90ebefb65947408fe7253a2b2af0bd833af1
hernancur <hernan.cur@hotmail.com>
2023-01-17 09:16:19 -0300

    modificacion-01

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function BinarioADecimal(num) {
+   
+}
+
+function DecimalABinario(num) {
+   //code
+}
+
+// .parseInt() 
+// .toString()
+
+module.exports = {
+   BinarioADecimal,
+   DecimalABinario,
+};
